c036cda175fd6c0cb93dec7e714e7e8c3835fae2
Budi Priyonggo <onggopro@gmail.com>
2025-02-09 21:29:12 +0700

    lupa password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,5 @@
 @extends('layouts.guest')
 @section('content')
-    {{-- <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="card shadow-lg " style="width: 70%; max-width: 1000px;">
-            <div class="row g-0">
-                <!-- Sisi Kiri: Gambar -->
-                <div class="col-md-6 d-none d-md-block">
-                    <img src="{{ asset('img/img-carousel-promo/onigasima.jpg') }}" alt="Login Image"
-                        class="img-fluid rounded-start" style="height: 100%; object-fit: cover;">
-                </div>
-                <!-- Sisi Kanan: Form Login -->
-                <div class="col-md-6">
-                    <div class="card-body p-4">
-                        <!-- Logo -->
-                        <a href="{{ asset('style/src/html/index.html') }}"
-                            class="text-nowrap logo-img text-center d-block mb-4">
-                            <img src="{{ asset('style/src/assets/images/logos/dark-logo.svg') }}" width="180"
-                                alt="Logo">
-                        </a>
-                        <h5 class="card-title text-center mb-3">Sign In to Your Account</h5>
-
-                        <!-- Session Status -->
-                        @if (session('status'))
-                            <div class="alert alert-success mb-4" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <!-- Email Address -->
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" id="email" name="email"
-                                    class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
-                                    autofocus autocomplete="username">
-                                @error('email')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <!-- Password -->
-                            <div class="mb-4">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" id="password" name="password"
-                                    class="form-control @error('password') is-invalid @enderror"
-                                    autocomplete="current-password">
-                                @error('password')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <!-- Remember Me -->
-                            <div class="form-check mb-4">
-                                <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
-                                <label class="form-check-label" for="remember_me">Remember this Device</label>
-                            </div>
-
-                            <!-- Submit Button -->
-                            <button type="submit" class="btn btn-primary w-100 py-2 mb-4">Sign In</button>
-
-                            <!-- Additional Links -->
-                            <div class="d-flex justify-content-between">
-                                @if (Route::has('password.request'))
-                                    <a class="text-primary fw-bold" href="{{ route('password.request') }}">Forgot
-                                        Password?</a>
-                                @endif
-                                <a class="text-primary fw-bold ms-2" href="{{ route('register') }}">Create an account</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
     <div class="flex justify-center items-center min-h-screen p-4"
         style="background: radial-gradient(135.69% 188.95% at 53.72% 47.02%, #0085FF 0%, #003465 64.5%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
 ">
@@ -85,16 +10,19 @@
             <img src="{{ asset('img/img-auth/2.png') }}" alt="Login Image" class="w-56 md:w-95 animate-pulse hidden md:block">
         </div>
         <div class="absolute top-0 start-80">
-            <img src="{{ asset('img/img-auth/6.png') }}" alt="Login Image" class="w-60 md:w-95 animate-pulse hidden md:block">
+            <img src="{{ asset('img/img-auth/6.png') }}" alt="Login Image"
+                class="w-60 md:w-95 animate-pulse hidden md:block">
         </div>
         <div class="absolute top-0 end-20">
             <img src="{{ asset('img/img-auth/4.png') }}" alt="Login Image" class="w-60 md:w-95 animate-pulse ">
         </div>
         <div class="absolute bottom-36 end-72">
-            <img src="{{ asset('img/img-auth/5.png') }}" alt="Login Image" class="w-60 md:w-95 animate-pulse hidden md:block">
+            <img src="{{ asset('img/img-auth/5.png') }}" alt="Login Image"
+                class="w-60 md:w-95 animate-pulse hidden md:block">
         </div>
         <div class="absolute bottom-0 end-20">
-            <img src="{{ asset('img/img-auth/1.png') }}" alt="Login Image" class="w-60 md:w-95 animate-pulse hidden md:block">
+            <img src="{{ asset('img/img-auth/1.png') }}" alt="Login Image"
+                class="w-60 md:w-95 animate-pulse hidden md:block">
         </div>
         <div class="px-14 py-10 pb-5 rounded-md  w-full md:w-1/3 flex flex-col items-center z-20"
             style="background: linear-gradient(148.46deg, rgba(255, 255, 255, 0.3) -47.18%, rgba(255, 255, 255, 0.11) 131%);
@@ -131,11 +59,11 @@
                     <button type="submit"
                         class="block w-full py-2 text-sm text-white bg-blue-700 rounded-md">Login</button>
                 </form>
-
                 <p class="text-white text-xs mt-10">
-                    Don’t have an account yet? <a href="{{ route('register') }}" class="font-bold text-white">Register</a>
-                    for free
+                    Dont have an Account? <a href="{{ route('register') }}" class="font-bold text-white">Register</a> or
+                    <a href="{{ route('password.request') }}" class="font-bold text-white">Forgot Your Password</a>
                 </p>
+
             </div>
         </div>
     </div>
