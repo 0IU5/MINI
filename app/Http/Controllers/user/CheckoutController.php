0dadf506c97749aaa5a7d4690446dd69c5e2ae70
Saifululum <164288569+Saifululum@users.noreply.github.com>
2025-02-11 07:12:41 +0700

    Update CheckoutController.php

--- app/Http/Controllers/user/CheckoutController.php
+++ app/Http/Controllers/user/CheckoutController.php
@@ -228,7 +228,7 @@ class CheckoutController extends Controller
 
         Payment::create([
             'order_id' => $order->id,
-            'status' => 'success',
+            'status' => 'pending',
         ]);
 
         return response()->json(['status' => 'success', 'snap_token' => $snapToken]);
